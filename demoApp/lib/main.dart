dc1716fcdfeabd80a7393268f6218e7d0224ee22
elvinosmanov <49588906+elvinosmanov@users.noreply.github.com>
2020-08-16 15:23:44 +0400

    last

--- demoApp/lib/main.dart
+++ demoApp/lib/main.dart
@@ -12,11 +12,20 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Contrary to popular belief, Lorem Ipsum is not simply random text',
+    'It has roots in a piece of classical Latin literature from 45 BC,',
+    'making it over 2000 years old. Richard McClintock, a Latin',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("QuoteList"),
+        title: Text("Awesome Quotes"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: quotes.map((quote) => Text(quote)).toList(),
       ),
     );
   }
