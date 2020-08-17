982989ef2b125e459e042225b3eca45dee0a4b88
elvinosmanov <49588906+elvinosmanov@users.noreply.github.com>
2020-08-17 13:07:18 +0400

    Extracting-Widget

--- demoApp/lib/main.dart
+++ demoApp/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:demoApp/quote.dart';
 import 'package:flutter/material.dart';
+import 'QuoteCard.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -25,37 +26,6 @@ class _QuoteListState extends State<QuoteList> {
         author: "Nermin Osmanova"),
   ];
 
-  Widget quoteTemplate(Quote quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              quote.quote,
-              style: TextStyle(
-                color: Colors.grey[600],
-                fontSize: 18.0,
-              ),
-            ),
-            SizedBox(
-              height: 6.0,
-            ),
-            Text(
-              quote.author,
-              style: TextStyle(
-                color: Colors.grey[800],
-                fontSize: 14.0,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,7 +35,7 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
       ),
     );
   }
