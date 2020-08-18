8f02d41683a0fe669389e5708a37de618343fecd
elvinosmanov <49588906+elvinosmanov@users.noreply.github.com>
2020-08-18 10:48:02 +0400

    changes

--- demoApp/lib/main.dart
+++ demoApp/lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:demoApp/pages/choose_location.dart';
+import 'package:demoApp/pages/home.dart';
+import 'package:demoApp/pages/loading.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    
+    initialRoute: "/home",
+    routes: {
+      "/": (context) => Loading(),
+      "/home": (context) => Home(),
+      "location": (context) => ChooseLocation(),
+    },
   ));
 }
