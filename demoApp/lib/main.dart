55c9e8b13cd7829891433f3ca620e85d95bbe29c
elvinosmanov <49588906+elvinosmanov@users.noreply.github.com>
2020-08-22 15:27:33 +0400

    changes

--- demoApp/lib/main.dart
+++ demoApp/lib/main.dart
@@ -2,6 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    
+    home: MyHome(),
   ));
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          "BUGUN NE YESEM?",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Image.asset('assets/space-3.jpg'),
+          ),
+        ],
+      ),
+    );
+  }
 }
